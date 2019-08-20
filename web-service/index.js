b5d7b13962c9a7c5fbb3c4937bc20b4242062321
Valeria Madaev <valeriamadaev@gmail.com>
2019-08-20 19:39:09 +0300

    Adding health-check

--- web-service/index.js
+++ web-service/index.js
@@ -2,6 +2,10 @@ const express= require("express");
 const app = express();
 require("./routers/results.router").defineRoutes(app);
 
+app.get('/health-check', (req,res)=>{
+    res.sendStatus(200);
+})
+
 app.listen(8080, error =>{
     if(error){
        console.error("Faild to run server: "+error);
