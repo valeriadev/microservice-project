72e92d36f9d652017e12eb93ff53e7fb79d3d6bd
Valeria Madaev <valeriamadaev@gmail.com>
2019-08-20 20:59:09 +0300

    adding db connection to .env

--- process-service/db/index.js
+++ process-service/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const item = require("./item.model")
-const sequelize = new Sequelize('mysql://admin:7QY]q[>989VV~FU=@main-db.cc4vgf6fgow7.eu-west-1.rds.amazonaws.com:3306/main');
+const sequelize = new Sequelize(process.env.db_connection);
 
 const itemModel = item.defineModel(Sequelize,sequelize);
 sequelize.sync({force:false})
